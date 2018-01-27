f89e9574bf485b269a41b23b6993f590033daa2f
Sofia Brown <ruthsofiab@gmail.com>
2018-01-27 00:46:31 -0600

    Avances Koreographer con Luces e Inputs

---
+++ Assets/Tests/Sofia/STD_KEventSubscriber.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class STD_KEventSubscriber : MonoBehaviour {
+
+    public Light colorLight;
+
+    private void Start()
+    {
+        Koreographer.Instance.RegisterForEvents("STD_Track_A", ChangeLightColor);
+    }
+
+    public void ChangeLightColor(KoreographyEvent koreoEvent)
+    {
+        if(koreoEvent.HasColorPayload())
+        {
+            colorLight.color = koreoEvent.GetColorValue();
+        }
+        else if(koreoEvent.HasTextPayload())
+        {
+
+        }
+    }
+}

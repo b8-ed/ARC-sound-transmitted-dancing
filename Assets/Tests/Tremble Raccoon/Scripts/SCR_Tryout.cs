82cca858b0455331fab352dbf8953a60be223388
DiegoADB <diegoadb17@gmail.com>
2018-01-28 09:18:25 -0600

    Its Alive
    MUAHSUHAUSHAUSHUAHSUHAUSHAUSHUAHSUAHSU

---
+++ Assets/Tests/Tremble Raccoon/Scripts/SCR_Tryout.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Tryout : MonoBehaviour {
+    public Rigidbody[] bone;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            int randomNum = Random.Range(0, bone.Length);
+            bone[randomNum].AddForce((Vector3.forward + Vector3.up) * 50);
+        }
+	}
+}
